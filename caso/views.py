cb1faccadc38903cc62a008cc605500463822741
Gustavo Marques <gustavomarques.gml@gmail.com>
2019-08-04 11:35:05 -0300

    Unactive post method

--- caso/views.py
+++ caso/views.py
@@ -8,5 +8,7 @@ class CasoViewSet(viewsets.ModelViewSet):
     queryset = Caso.objects.all()
     serializer_class = CasoSerializer
     filter_backends = [filters.SearchFilter]
+    #http_method_names = ['get', 'post', 'head', 'put']
+    http_method_names = ['get', 'head']
     search_fields = ['titulo']
 # Create your views here.
